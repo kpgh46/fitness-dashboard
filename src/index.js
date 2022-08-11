f34562c18b678b53e0ff9b6238ab517067cb703f
Kevin McPeak <kevmc46@gmail.com>
2022-08-11 14:17:34 -0400

    last setup test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import "./style.css";
+
 let tDiv = document.createElement("div");
-tDiv.textContent = "jskfldk";
+tDiv.textContent = "This is a test";
 
 document.getElementById("test").appendChild(tDiv);
