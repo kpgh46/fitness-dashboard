341cab8829f5956522ab73f90a31f1b78e943031
Kevin McPeak <kevmc46@gmail.com>
2022-08-17 13:16:14 -0400

    added dataID to bottom cells

--- src/Render.js
+++ src/Render.js
@@ -2,16 +2,17 @@ let render = (workout) => {
 	let bottomContainer = document.querySelector("#workout-container");
 	bottomContainer.innerHTML = "";
 
-	workout.forEach((week) => {
+	workout.forEach((week, weeki) => {
 		let bottomRow = document.createElement("section");
 		bottomRow.classList.add("row");
 		bottomRow.classList.add("bottom");
 		bottomRow.id = "days";
 		bottomRow.style = "height: 10vw";
-		week.forEach((day) => {
+		week.forEach((day, dayi) => {
 			let bottomCell = document.createElement("div");
 			bottomCell.classList.add("col-2");
 			bottomCell.classList.add("completed");
+			bottomCell.dataset.id = `${weeki}${dayi}`;
 			bottomCell.innerHTML = `${day[0].bodyPart} & ${day[1].bodyPart}`;
 			bottomRow.appendChild(bottomCell);
 		});
